904903448cecd68d5d3caae37296767907d878c8
Abdallah-AlThwieb <aldwybballh@gmail.com>
2024-05-04 13:57:48 +0300

    Create and list projects

--- projects/models.py
+++ projects/models.py
@@ -24,7 +24,7 @@ class Project(models.Model):
     created_at = models.DateTimeField(auto_now_add=True)
     updated_at = models.DateTimeField(auto_now=True)
     category = models.ForeignKey(Category, on_delete=models.PROTECT)
-    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.PROTECT)
+    user = models.ForeignKey(AUTH_USER_MODEL, on_delete=models.PROTECT, null=True)
 
     def __str__(self):
         return self.title
