5b596d71f874616717a39127dadf993b0c23db2c
Abdallah-AlThwieb <aldwybballh@gmail.com>
2024-05-08 14:33:24 +0300

    Search and pagination

--- projects_management/settings.py
+++ projects_management/settings.py
@@ -37,6 +37,8 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
+    'django.contrib.humanize',
+    'debug_toolbar',
     'projects',
     'accounts',
 ]
@@ -127,3 +129,9 @@ STATIC_URL = 'static/'
 # https://docs.djangoproject.com/en/5.0/ref/settings/#default-auto-field
 
 DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
+
+INTERNAL_IPS = [
+    # ...
+    '127.0.0.1'
+    # ...
+]
