0840688b46e061bfc40332c614e48f366efeaf4e
Abdallah-AlThwieb <aldwybballh@gmail.com>
2024-05-10 14:28:41 +0300

    Registration & profile update

--- accounts/urls.py
+++ accounts/urls.py
@@ -1,9 +1,12 @@
 from django.contrib.auth.views import LoginView, LogoutView
 from django.urls import path, include
 from .forms import UserLoginForm
+from .views import RegisterView, edit_profile
 
 urlpatterns = [
     path('login/', LoginView.as_view(authentication_form= UserLoginForm), name='login'),
     path('logout/', LogoutView.as_view(), name='logout'),
+    path('register/', RegisterView.as_view(), name='register'),
+    path('profile/', edit_profile, name='profile'),
     path('', include('django.contrib.auth.urls')),
 ]
